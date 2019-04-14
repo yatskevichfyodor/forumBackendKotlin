b1af26cfb264ec0b0cdbc9b57f01e49ff98aa9bd
fedor <yatskevichfyodor@gmail.com>
2019-04-14 22:07:05 +0300

    Topic improved

--- src/main/kotlin/base/forum/model/Topic.kt
+++ src/main/kotlin/base/forum/model/Topic.kt
@@ -3,20 +3,22 @@ package base.forum.model
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.util.*
 import javax.persistence.*
+import javax.persistence.CascadeType.PERSIST
+import javax.persistence.TemporalType.TIMESTAMP
 
 @Entity
-@Table(name="topics")
-data class Topic (
+@Table(name = "topics")
+data class Topic(
         @Id
         @GeneratedValue
         val id: Long = 0,
 
         val title: String = "",
 
-        @Temporal(TemporalType.TIMESTAMP)
+        @Temporal(TIMESTAMP)
         val timestamp: Date = Date(),
 
         @JsonIgnore
-        @OneToMany(mappedBy = "topic")
-        val comments: List<Comment>? = null
+        @OneToMany(mappedBy = "topic", cascade = [PERSIST])
+        val comments: List<Comment> = listOf()
 )
